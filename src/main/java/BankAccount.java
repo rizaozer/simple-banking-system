94de1ee6096941a26fdc802650817dfa698309d0
rizaozer <rizaoozer@gmail.com>
2022-02-23 22:36:03 +0200

    add more functionality to luhn algorithm

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -19,17 +19,39 @@ public class BankAccount {
         String k = "400000";
         String l = valueOf(10000 + new Random().nextInt(99999));
         String m = valueOf(1000 + new Random().nextInt(9999));
-        return k + l + m;
+        String lastNumber = String.valueOf(findTheLastNumber(k + l + m));
+        return k + l + m + lastNumber;
     }
 
     public static int findTheLastNumber(String cardNumber) {
-        int[] numbersArray = new int[cardNumber.length()];
+        int[] numbers = new int[cardNumber.length()];
 
         for(int i = 0; i < cardNumber.length(); i++){
-            numbersArray[i] = Character.getNumericValue(cardNumber.charAt(i));
+            numbers[i] = Character.getNumericValue(cardNumber.charAt(i));
+        }
+
+        for (int i = 0; i < 15; i++) {
+            if(i % 2 == 0) {
+                if(numbers[i] * 2 > 9) {
+                    numbers[i] = (numbers[i] * 2) - 9;
+                } else {
+                    numbers[i] = numbers[i] * 2;
+                }
+            }
+        }
+        return generateLastNumber(numbers);
+    }
+
+    public static int generateLastNumber(int [] numbers) {
+
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        if(sum % 10 != 0) {
+            return 10 - sum % 10;
         }
         return 0;
-
     }
 
     public static String generatePin() {
