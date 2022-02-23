4a428efec6628e9485121ff9eda1a70c84eacefb
rizaozer <rizaoozer@gmail.com>
2022-02-23 23:03:43 +0200

    add comments to luhn algorithm

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import static java.lang.String.valueOf;
@@ -15,45 +13,43 @@ public class BankAccount {
         this.cardPin = generatePin();
     }
 
+    // To generate credit card number
     public static String generateCardNumber() {
         String k = "400000";
         String l = valueOf(10000 + new Random().nextInt(99999));
         String m = valueOf(1000 + new Random().nextInt(9999));
-        String lastNumber = String.valueOf(findTheLastNumber(k + l + m));
-        return k + l + m + lastNumber;
+        return k + l + m + findTheLastNumber(k + l + m);
     }
 
+    // find the last number by using luhn algorithm
     public static int findTheLastNumber(String cardNumber) {
+
         int[] numbers = new int[cardNumber.length()];
 
         for(int i = 0; i < cardNumber.length(); i++){
             numbers[i] = Character.getNumericValue(cardNumber.charAt(i));
         }
 
-        for (int i = 0; i < 15; i++) {
-            if(i % 2 == 0) {
+        for (int i = 0; i < 15; i += 2) {
                 if(numbers[i] * 2 > 9) {
                     numbers[i] = (numbers[i] * 2) - 9;
                 } else {
                     numbers[i] = numbers[i] * 2;
                 }
-            }
         }
         return generateLastNumber(numbers);
     }
 
+    // To generate last number after luhn algorithm
     public static int generateLastNumber(int [] numbers) {
-
         int sum = 0;
         for (int number : numbers) {
             sum += number;
         }
-        if(sum % 10 != 0) {
-            return 10 - sum % 10;
-        }
-        return 0;
+        return (sum % 10 != 0) ?  10 - sum % 10 :  0;
     }
 
+    // To generate credit card number
     public static String generatePin() {
         return valueOf(1000 + new Random().nextInt(9999));
     }
